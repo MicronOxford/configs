ba1530607657e75304eb622a9f2ca0abb516c188
Carnë Draug <carandraug+dev@gmail.com>
2020-11-20 13:06:40 +0000

    development/microscope-devices.py: update for TestFilterWheel recent changes.

--- development/microscope-devices.py
+++ development/microscope-devices.py
@@ -8,8 +8,10 @@ DEVICES = [
     device(testdevices.TestCamera, '127.0.0.1', 8001),
     device(testdevices.TestLaser, '127.0.0.1', 8002),
     device(testdevices.TestLaser, '127.0.0.1', 8003),
-    device(testdevices.TestFilterWheel, '127.0.0.1', 8004),
-    device(testdevices.TestFilterWheel, '127.0.0.1', 8005),
+    device(testdevices.TestFilterWheel, '127.0.0.1', 8004,
+           {'positions': 6}),
+    device(testdevices.TestFilterWheel, '127.0.0.1', 8005,
+           {'positions': 12}),
     device(testdevices.DummyDSP, '127.0.0.1', 8006),
     device(testdevices.DummySLM, '127.0.0.1', 8007),
 ]
