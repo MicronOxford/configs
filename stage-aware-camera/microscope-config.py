32726f168359de04863b5271f2303185fca3c746
Carnë Draug <carandraug+dev@gmail.com>
2020-12-15 17:34:58 +0000

    stage-aware-camera: fix import now that it is on master

--- stage-aware-camera/microscope-config.py
+++ stage-aware-camera/microscope-config.py
@@ -1,7 +1,7 @@
 from microscope.device_server import device
-from microscope.testsuite.simulated_system import simulated_system_from_image
+from microscope.testsuite.simulated_setup import simulated_setup_from_image
 
 DEVICES = [
-    device(simulated_system_from_image, 'localhost', 8000,
+    device(simulated_setup_from_image, 'localhost', 8000,
            conf={'filepath': 'mosaicimage.tif'}),
 ]
