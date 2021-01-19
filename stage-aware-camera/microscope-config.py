0047f78a6564277b8434e1d5e36d8d7cc77dcadd
Ian Dobbie <ian.dobbie@bioch.ox.ac.uk>
2021-01-19 15:18:06 +0000

    updated stage-aware-camera config to reflect moved simulated devices in microscope

--- stage-aware-camera/microscope-config.py
+++ stage-aware-camera/microscope-config.py
@@ -1,5 +1,5 @@
 from microscope.device_server import device
-from microscope.testsuite.simulated_setup import simulated_setup_from_image
+from microscope.simulators.stage_aware_camera import simulated_setup_from_image
 
 DEVICES = [
     device(simulated_setup_from_image, 'localhost', 8000,
